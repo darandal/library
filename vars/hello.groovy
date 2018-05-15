a5be245897bbf15a810b72ba59e025ecc58c76cd
darandal <39315909+darandal@users.noreply.github.com>
2018-05-15 16:39:36 -0600

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,6 +2,8 @@
 def call() {
   sh 'echo Hello World'
   
+  def results = sh(script:"echo ${options}", returnStdout:true).trim()
+  echo "${results}"
   def varHere = "Something else"
   echo "${varHere}"
 }
